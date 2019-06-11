1103f190183dfcd38e7dbe34244b5fbc0fc4a616
Alex Sartan <asartan@pivotal.io>
2019-06-11 14:46:42 -0600

    Download synchronously

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Google.Api.Gax;
+using Google.Apis.Storage.v1.Data;
+using Google.Cloud.Storage.V1;
+using Object = System.Object;
+
+namespace landsat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // If/else dir cache:                114188.51ms, 189050.292ms
+            // If dir cache:                      38039.178ms
+            // No dir cache:                      35979.628ms, 37687.228ms
+            // No dir cache, no output:           34946.14ms
+            var startTime = DateTime.Now;
+            const int objectLimit = 2000;
+            const string downloadDir = "./data";
+            const string bucketName = "gcp-public-data-landsat";
+
+            DownloadFromGcs(downloadDir, bucketName, ".txt", objectLimit);
+
+            var duration = DateTime.Now - startTime;
+            Console.WriteLine($"Took {duration.TotalMilliseconds}ms to download {objectLimit} objects");
+            Environment.Exit(0);
+        }
+
+        private static void DownloadFromGcs(string downloadDir, string bucketName, string filter = ".txt",
+            int limit = 1000)
+        {
+            var storageClient = StorageClient.Create();
+            var objects = storageClient.ListObjects(bucketName);
+
+            if (objects == null)
+            {
+                return;
+            }
+
+            downloadDir = Path.GetFileName(downloadDir);
+
+            var objectCount = 0;
+
+            foreach (var o in objects)
+            {
+                if (objectCount == limit)
+                {
+                    break;
+                }
+
+                if (!o.Name.EndsWith(filter))
+                {
+                    continue;
+                }
+
+                var destinationFile = Path.Combine(downloadDir, o.Name);
+                var destinationDir = string.Join("/", destinationFile.Split("/").SkipLast(1));
+
+                // Blindly call even if directory already exists
+                Directory.CreateDirectory(destinationDir);
+
+                using (var outputFile = File.OpenWrite(destinationFile))
+                {
+                    storageClient.DownloadObject(bucketName, o.Name, outputFile);
+                }
+
+                objectCount++;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+1. Generate [service account key](https://console.cloud.google.com/apis/credentials)
+1. Run the app:
+`GOOGLE_APPLICATION_CREDENTIALS=/path/to/your/service-account-key dotnet run`
+
+### Resources:
+- https://cloud.google.com/storage/docs/listing-objects
+- https://cloud.google.com/storage/docs/gsutil/addlhelp/HowSubdirectoriesWork
